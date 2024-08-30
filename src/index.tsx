a7fc7859173ef7518ea1da6ae0b69e30bd1494cb
Aoufi Abderhmane <Abdouou7@gmail.com>
2024-08-30 11:21:18 +0100

    add custom navigation provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import AmityUiKitProvider from './providers/amity-ui-kit-provider';
 import RecentChat from './screens/RecentChat/RecentChat';
 import AmityUiKitChat from './routes/ChatNavigator';
+import Ila26AmityUiKitChat from './ila26/routes/Ila26Navigator';
 
-export {
-  AmityUiKitProvider,
-  RecentChat,
-  AmityUiKitChat,
-};
+export { AmityUiKitProvider, RecentChat, AmityUiKitChat, Ila26AmityUiKitChat };
